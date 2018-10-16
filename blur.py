6dc59540bd40cdff5cc24a37574e9ac0e318a15b
nickgouldleeds <44134361+nickgouldleeds@users.noreply.github.com>
2018-10-16 13:31:11 +0100

    Update blur.py

--- blur.py
+++ blur.py
@@ -1,28 +1,28 @@
 # blur ---------------------------------------
- import matplotlib.pyplot
+import matplotlib.pyplot
 
- data = []
- processed_data = []
+data = []
+processed_data = []
 
- # Fill with random data.
- for i in (range(0,99)):
-     datarow = []
-     for j in (range(0,99)):
-         datarow.append(random.randint(0,255))
-     data.append(datarow)
+# Fill with random data.
+for i in (range(0,99)):
+    datarow = []
+for j in (range(0,99)):
+    datarow.append(random.randint(0,255))
+    data.append(datarow)
 
  # Blur.
  for i in (range(0,99)):
      datarow = []
-     for j in (range(0,99)):
-         sum = data[i][j]
-         sum += data[i-1][j]
-         sum += data[i+1][j]
-         sum += data[i][j+1]
-         sum += data[i][j-1]
-         sum /= 5
-     datarow.append(sum)
-     processed_data.append(datarow)
+ for j in (range(0,99)):
+     sum = data[i][j]
+     sum += data[i-1][j]
+     sum += data[i+1][j]
+     sum += data[i][j+1]
+     sum += data[i][j-1]
+     sum /= 5
+ datarow.append(sum)
+ processed_data.append(datarow)
 
  matplotlib.pyplot.imshow(data)
  matplotlib.pyplot.show()
